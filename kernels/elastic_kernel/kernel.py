539058e43cd146f89b43f408fed79cbe9bd459c1
MRyutaro <m2002r1028@icloud.com>
2025-06-13 14:03:51 +0900

    fix

--- kernels/elastic_kernel/kernel.py
+++ kernels/elastic_kernel/kernel.py
@@ -46,6 +46,8 @@ class ElasticKernel(IPythonKernel):
 
         self.logger.info("===============================================")
         self.logger.info(f"Initializing ElasticKernel ({self.session.session})")
+        self.logger.info(f"Kernel ID: {self.session.kernel_id}")
+        self.logger.info(f"Session: {self.session}")
         self.logger.info("===============================================")
 
         # コマンドライン引数を取得
